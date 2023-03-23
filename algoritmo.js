20924b1c63eea665ef99db14a9490c22139f83ee
Nahir Martinez <nahirmartinez09@gmail.com>
2023-03-23 10:01:41 -0300

    segundo commit

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<summary>Informacion Importante</summary>
+<img height="400px" src="https://cienciaybiologia.com/wp-content/uploads/2017/06/panda-rojo-4.jpg"
+    alt="imagen panda rojo">
+<iframe height="400px" width="500px" src="https://es.wikipedia.org/wiki/Ailurus_fulgens"
+    frameborder="0"></iframe>
+<p> Informacion sobre pandas rojos</p>
+<a href="https://www.nationalgeographic.es/animales/panda-rojo">Link</a>
+
+`;
